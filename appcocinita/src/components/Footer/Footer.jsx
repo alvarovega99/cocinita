12567cafeeaac2d5d3408e636e05e4e5301726e4
Alvaro Vega <64806626+alvarovega99@users.noreply.github.com>
2022-09-01 13:04:23 -0300

    Update Footer.jsx

--- appcocinita/src/components/Footer/Footer.jsx
+++ appcocinita/src/components/Footer/Footer.jsx
@@ -26,7 +26,6 @@ const Footer = () => {
             <p>VISITS FROM FRIENDS</p>
         </div>
         <div className={style.bottomText}>
-            <p>PLEASE NOTE OUR FOOD MAY CONTAIN AND/OR SHARE EQUIPMENT WITH SOY, WHEAT, AND NUTS.<br/>GET MORE INFORMATION ON IMPOSSIBLE FOODS.</p>
             <p>COPYRIGHT © 2022 THE COCINITA</p>
         </div>
     </div>
